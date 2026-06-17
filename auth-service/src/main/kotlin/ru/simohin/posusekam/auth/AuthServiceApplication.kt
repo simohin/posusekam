eece61e2888247468ef0c61b66799a0edec36e79
Timofei Simokhin <timofei@simohin.ru>
2026-06-17 15:29:07 +0500

    fix(auth-service): resolve JPA entity scanning issue, optimize dockerfiles, update Helm properties

--- auth-service/src/main/kotlin/ru/simohin/posusekam/auth/AuthServiceApplication.kt
+++ auth-service/src/main/kotlin/ru/simohin/posusekam/auth/AuthServiceApplication.kt
@@ -1,9 +1,11 @@
 package ru.simohin.posusekam.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EntityScan("ru.simohin.posusekam")
 class AuthServiceApplication
 
 fun main(args: Array<String>) {
